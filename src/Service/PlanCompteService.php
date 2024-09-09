5b287ac24e9aabd98b3929ae0b650198de3bfe75
Miarotiana Stephan <miarotianastephan01@gmail.com>
2024-09-09 13:27:04 +0300

    ok insertion + doublon

--- src/Service/PlanCompteService.php
+++ src/Service/PlanCompteService.php
@@ -11,6 +11,10 @@ class PlanCompteService
 
     function insertHierarchy(EntityManagerInterface $entityManager, array $data, ?CompteMere $parent = null) {
         if ($parent === null) {
+            // Vérifier si le compte mère existe déjà
+            if ($this->accountExists($entityManager,$data['numero'], CompteMere::class)) {
+                return; // Ne pas insérer de doublon
+            }
             // Création de l'entité CompteMere pour la racine
             $compteMere = new CompteMere();
             $compteMere->setCptNumero($data['numero']);
@@ -25,6 +29,10 @@ class PlanCompteService
                 }
             }
         } else {
+            // Vérifier si le plan de compte existe déjà
+            if ($this->accountExists($entityManager ,$data['numero'], PlanCompte::class)) {
+                return; // Ne pas insérer de doublon
+            }
             // Création de l'entité PlanCompte pour les enfants
             $planCompte = new PlanCompte();
             $planCompte->setCptNumero($data['numero']);
@@ -41,12 +49,20 @@ class PlanCompteService
                 $compteMere->setCptLibelle($data['libelle']);
                 $entityManager->persist($compteMere);
                 $entityManager->flush(); // Flush pour obtenir l'ID du compte mère
-                
+
                 foreach ($data['enfants'] as $enfant) {
                     $this->insertHierarchy($entityManager, $enfant, $parent); // Enfants des enfants
                 }
             }
         }
+    }
+
+    private function accountExists(EntityManagerInterface $entityManager ,string $numero, string $entityClass): bool
+    {
+        $repository = $entityManager->getRepository($entityClass);
+        $existingAccount = $repository->findOneBy(['cpt_numero' => $numero]);
+
+        return $existingAccount !== null;
     }
 
 
